f38e336c03dafd1d3c39725f5f5549c8d272e4fb
Danila Lutsai <dlutsai98@gmail.com>
2026-07-19 18:14:23 +0300

    Practice file

---
+++ src/practice.ts
@@ -0,0 +1,12 @@
+// This is practice whiteboard for all the dirtiest code you could ever see in your life
+
+interface Transport {
+  wheels: number,
+  motor: string,
+  windows: number,
+  doors: number,
+  convertible?: true,
+  interior: 'Suede' | 'Fabric' | 'Leather'
+}
+
+
